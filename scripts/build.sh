3d979444f567e52c664afb0f263caf222223285e
Greg Buehler <gregory.r.buehler@gmail.com>
2020-06-25 23:49:11 -0700

    Create build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+WORKDIR="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)")"
+
+cat << EOF > $WORKDIR/local.properties
+takDebugKeyFile=$TAK_KEY_FILE_PATH
+takDebugKeyFilePassword=$TAK_KEY_FILE_PASSWD
+takDebugKeyAlias=$TAK_KEY_ALIAS
+takDebugKeyPassword=$TAK_KEY_PASSWD
+takReleaseKeyFile=$TAK_KEY_FILE_PATH
+takReleaseKeyFilePassword=$TAK_KEY_FILE_PASSWD
+takReleaseKeyAlias=$TAK_KEY_ALIAS
+takReleaseKeyPassword=$TAK_KEY_PASSWD
+EOF
+
+cd $WORKDIR/atak && ./gradlew assemble
