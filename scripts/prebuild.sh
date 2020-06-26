80fa7da48dd19bbf8f90e273255b11785ceeda54
Greg Buehler <gregory.r.buehler@gmail.com>
2020-06-25 21:33:29 -0700

    Update prebuild.sh

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -1,25 +1,27 @@
 #!/bin/sh
 
-(cd .. && \
+WORKDIR="$(dirname "$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)")"
+
+(cd $WORKDIR && \
         gzip -d ./depends/gdal-2.2.3-mod.tar.gz && \
         cp depends/gdal-2.2.3-mod.tar . && \
         tar xf gdal-2.2.3-mod.tar)
 
-(cd ../takthirdparty && make TARGET=android-armeabi-v7a \
+(cd $WORKDIR/takthirdparty && make TARGET=android-armeabi-v7a \
 	build_spatialite \
 	build_commoncommo \
 	build_gdal \
 	build_assimp) &
-(cd ../takthirdparty && make TARGET=android-arm64-v8a \
+(cd $WORKDIR/takthirdparty && make TARGET=android-arm64-v8a \
 	build_spatialite \
 	build_commoncommo \
 	build_gdal \
 	build_assimp) &
-(cd ../takthirdparty && make TARGET=android-x86 \
+(cd $WORKDIR/takthirdparty && make TARGET=android-x86 \
 	build_spatialite \
 	build_commoncommo \
 	build_gdal \
 	build_assimp) &
 wait
 
-(cd ../takengine/thirdparty && git clone https://github.com/synesissoftware/STLSoft-1.9.git stlsoft)
+(mkdir -p $WORKDIR/takengine/thirdparty && cd $WORKDIR/takengine/thirdparty && git clone https://github.com/synesissoftware/STLSoft-1.9.git stlsoft)
